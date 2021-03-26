2ad1c825982f4afb61955951105eb407ba49da54
smackov <smackovdinislam@gmail.com>
2021-03-27 00:42:14 +0500

    Add tests to api and serializers for steps #1, #2

--- delivery/tests/test_api.py
+++ delivery/tests/test_api.py
@@ -2,6 +2,8 @@
 Test api.
 """
 
+from datetime import time
+
 from rest_framework.test import APITestCase
 from django.urls import reverse
 from rest_framework import status
@@ -9,7 +11,10 @@ from rest_framework import status
 from ..models import Courier, Region, WorkingHours
 
 
-class CourierTestAPITestCase(APITestCase):
+class CourierListAPITestCase(APITestCase):
+    """
+    The test case for CourierListAPI class.
+    """
     
     def setUp(self):
         
@@ -29,36 +34,26 @@ class CourierTestAPITestCase(APITestCase):
         }
         self.input_many_data = [self.input_data_foot, self.input_data_bike]
 
-    def test_post_one_valide_courier(self):
+    def test_post_one_valid_courier(self):
         url = reverse('couriers')
         self.input_data['data'].append(self.input_data_foot)
         response = self.client.post(url, self.input_data, format='json')
-        
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)       
+        self.assertEqual(response.data, {'couriers': [{'id': 1}]})
         
         # Test courier instance
-        courier = Courier.objects.get()
         self.assertEqual(Courier.objects.count(), 1)
+        courier = Courier.objects.get()
         self.assertEqual(courier.courier_id, 1)
         self.assertEqual(courier.courier_type, 'foot')
-        
-        # Test region instances
-        self.assertCountEqual(courier.regions.all(), Region.objects.all())
-        self.assertEqual(Region.objects.all()[0].id, 1)
-        self.assertEqual(Region.objects.all()[1].id, 2)
-        
-        # Test working_hours instances
-        self.assertCountEqual(courier.working_hours.all(), WorkingHours.objects.all())
-        self.assertEqual(str(WorkingHours.objects.all()[0]), "09:00-18:00")
-        self.assertEqual(str(WorkingHours.objects.all()[1]), "20:00-22:10")
 
-    def test_post_two_valide_courier(self):
+    def test_post_two_valid_couriers(self):
         url = reverse('couriers')
         self.input_data['data'].append(self.input_data_foot)
         self.input_data['data'].append(self.input_data_bike)
         response = self.client.post(url, self.input_data, format='json')
-        
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)       
+        self.assertEqual(response.data, {'couriers': [{'id': 1}, {'id': 2}]})
         
         # Test courier instances
         courier_foot = Courier.objects.get(courier_id=1)
@@ -68,20 +63,69 @@ class CourierTestAPITestCase(APITestCase):
         self.assertEqual(courier_bike.courier_id, 2)
         self.assertEqual(courier_foot.courier_type, 'foot')
         self.assertEqual(courier_bike.courier_type, 'bike')
+
+    def test_post_two_invalid_couriers(self):
         
-        # Test region instances
-        self.assertCountEqual(courier_foot.regions.all(), Region.objects.filter(id__in=[1, 2]))
-        self.assertCountEqual(courier_bike.regions.all(), Region.objects.filter(id__in=[2, 3]))
-        self.assertEqual(Region.objects.all()[0].id, 1)
-        self.assertEqual(Region.objects.all()[1].id, 2)
-        self.assertEqual(Region.objects.all()[2].id, 3)
+        self.input_data_foot['courier_type'] = 'none'
+        self.input_data_bike['courier_type'] = 'none'
         
-        # Test working_hours instances
-        self.assertEqual(WorkingHours.objects.count(), 3)
-        self.assertEqual(str(WorkingHours.objects.all()[0]), "09:00-18:00")
-        self.assertEqual(WorkingHours.objects.all()[0].courier, courier_foot)
-        self.assertEqual(str(WorkingHours.objects.all()[1]), "20:00-22:10")
-        self.assertEqual(WorkingHours.objects.all()[1].courier, courier_foot)
-        self.assertEqual(str(WorkingHours.objects.all()[2]), "10:00-14:00")
-        self.assertEqual(WorkingHours.objects.all()[2].courier, courier_bike)
+        self.input_data['data'].append(self.input_data_foot)
+        self.input_data['data'].append(self.input_data_bike)
+        url = reverse('couriers')
+        response = self.client.post(url, self.input_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)  
+        self.assertEqual(Courier.objects.count(), 0)
+        self.assertEqual(Region.objects.count(), 0)
+        self.assertEqual(WorkingHours.objects.count(), 0)
+ 
+
+class CourierItemAPITestCase(APITestCase):
+    """
+    The test case for CourierItemAPI class.
+    """
+    
+    def setUp(self):
         
+        # Input data
+        self.input_data = {'data': []}
+        self.input_data_foot = {
+            'courier_id': 1,
+            'courier_type': 'foot',
+            'regions': [1, 2],
+            'working_hours': ["09:00-18:00", "20:00-22:10"]
+        }
+        self.input_data_bike = {
+            'courier_id': 2,
+            'courier_type': 'bike',
+            'regions': [2, 3],
+            'working_hours': ["10:00-14:00"]
+        }
+        self.input_many_data = [self.input_data_foot, self.input_data_bike]
+        
+        # Create regions: 1, 2, 3, 4, 5
+        for region_number in range(1, 6):
+            region = Region.objects.create(id=region_number)
+            setattr(self, f'region_{region_number}', region)
+
+        # Create courier
+        self.courier = Courier.objects.create(
+            courier_id=1, courier_type='car')
+        self.courier.regions.set([1, 2, 3])
+
+        # Create working hours
+        self.working_hours_1 = WorkingHours.objects.create(
+            start=time(hour=9), end=time(hour=12), courier=self.courier)
+        self.working_hours_2 = WorkingHours.objects.create(
+            start=time(hour=18), end=time(hour=20), courier=self.courier)
+
+    def test_changed_all_courier_fields(self):
+        url = reverse('courier-item', args=[1])
+        response = self.client.patch(url, self.input_data_foot, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)       
+        self.assertEqual(response.data, self.input_data_foot)
+
+    def test_request_with_invalid_courier_type(self):
+        url = reverse('courier-item', args=[1])
+        self.input_data_foot['courier_type'] = 'zuzu'
+        response = self.client.patch(url, self.input_data_foot, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)         
--- delivery/tests/test_serializers.py
+++ delivery/tests/test_serializers.py
@@ -16,17 +16,18 @@ class CourierItemPostSerializerTestCase(TestCase):
     """
 
     def setUp(self):
-        # Input data for serializer
+        
+        # Input data
         self.input_data_foot = {
             'courier_id': 1,
             'courier_type': 'foot',
             'regions': [1, 2],
-            'working_hours': ["09:00-18:00", "20:00-22:00"]
+            'working_hours': ["09:00-18:00", "20:00-22:10"]
         }
         self.input_data_bike = {
             'courier_id': 2,
             'courier_type': 'bike',
-            'regions': [3, 4],
+            'regions': [2, 3],
             'working_hours': ["10:00-14:00"]
         }
         self.input_many_data = [self.input_data_foot, self.input_data_bike]
@@ -43,3 +44,56 @@ class CourierItemPostSerializerTestCase(TestCase):
         couriers = serializer.save()
         expected_couriers = Courier.objects.filter(courier_id__in=[1, 2])
         self.assertCountEqual(couriers, expected_couriers)
+
+    def test_save_one_valide_courier(self):
+        serializer = CourierItemPostSerializer(
+            data=[self.input_data_foot], many=True)
+        self.assertTrue(serializer.is_valid())
+        couriers = serializer.save()
+        
+        # Test courier instance
+        self.assertEqual(Courier.objects.count(), 1)
+        courier = Courier.objects.get()
+        self.assertEqual(courier.courier_id, 1)
+        self.assertEqual(courier.courier_type, 'foot')
+        
+        # Test region instances
+        self.assertCountEqual(courier.regions.all(), Region.objects.all())
+        self.assertEqual(Region.objects.all()[0].id, 1)
+        self.assertEqual(Region.objects.all()[1].id, 2)
+        
+        # Test working_hours instances
+        self.assertCountEqual(courier.working_hours.all(), WorkingHours.objects.all())
+        self.assertEqual(str(WorkingHours.objects.all()[0]), "09:00-18:00")
+        self.assertEqual(str(WorkingHours.objects.all()[1]), "20:00-22:10")
+
+    def test_save_two_valide_courier(self):
+        serializer = CourierItemPostSerializer(
+            data=self.input_many_data, many=True)
+        self.assertTrue(serializer.is_valid())
+        serializer.save()
+        
+        # Test courier instances
+        courier_foot = Courier.objects.get(courier_id=1)
+        courier_bike = Courier.objects.get(courier_id=2)
+        self.assertEqual(Courier.objects.count(), 2)
+        self.assertEqual(courier_foot.courier_id, 1)
+        self.assertEqual(courier_bike.courier_id, 2)
+        self.assertEqual(courier_foot.courier_type, 'foot')
+        self.assertEqual(courier_bike.courier_type, 'bike')
+        
+        # Test region instances
+        self.assertCountEqual(courier_foot.regions.all(), Region.objects.filter(id__in=[1, 2]))
+        self.assertCountEqual(courier_bike.regions.all(), Region.objects.filter(id__in=[2, 3]))
+        self.assertEqual(Region.objects.all()[0].id, 1)
+        self.assertEqual(Region.objects.all()[1].id, 2)
+        self.assertEqual(Region.objects.all()[2].id, 3)
+        
+        # Test working_hours instances
+        self.assertEqual(WorkingHours.objects.count(), 3)
+        self.assertEqual(str(WorkingHours.objects.all()[0]), "09:00-18:00")
+        self.assertEqual(WorkingHours.objects.all()[0].courier, courier_foot)
+        self.assertEqual(str(WorkingHours.objects.all()[1]), "20:00-22:10")
+        self.assertEqual(WorkingHours.objects.all()[1].courier, courier_foot)
+        self.assertEqual(str(WorkingHours.objects.all()[2]), "10:00-14:00")
+        self.assertEqual(WorkingHours.objects.all()[2].courier, courier_bike)
