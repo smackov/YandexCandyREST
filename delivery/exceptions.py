245754e5dd40346e2dcacc7dce48a649fa61175e
smackov <smackovdinislam@gmail.com>
2021-03-29 22:48:10 +0500

    Add exception handlers

--- delivery/exceptions.py
+++ delivery/exceptions.py
@@ -5,7 +5,14 @@ The utils.
 from rest_framework.exceptions import APIException
 from rest_framework.exceptions import ValidationError
 from rest_framework.views import exception_handler
+from django.urls import reverse
 
+
+class OrderAssignBadRequest(APIException):
+    status_code = 400
+    default_detail = "Courier doesn't exist"
+    default_code = 'Bad request'
+    
 
 def validate_exception_handler(exc, context):
     """
@@ -20,17 +27,60 @@ def validate_exception_handler(exc, context):
 
     # check that a ValidationError exception is raised
     if isinstance(exc, ValidationError):
-        # Wrap all response data (errors messages) into dict
-        # with key 'validation_error
-        response.data = {
-            'validation_error': {'couriers': response.data},
-        }
+        path = context['request'].stream.path
+        if path == reverse('couriers'):
+            response = couriers_exception_handler(response, context)
+        elif path == reverse('orders'):
+            response = orders_exception_handler(response, context)
 
     return response
 
 
-class OrderAssignBadRequest(APIException):
-    status_code = 400
-    default_detail = "Courier doesn't exist"
-    default_code = 'Bad request'
+def couriers_exception_handler(response, context):
+    """
+    The courier exception handler.
+    """
+    
+    request_data = context['request'].data['data']
+    
+    # Create new formated data
+    formated_data = []
+    for courier_data, initial_errors in zip(request_data, response.data):
+        courier_id = courier_data.get('courier_id')
+        if courier_id and initial_errors:
+            error_dict = {'id': courier_id}
+            error_dict.update(initial_errors)
+            formated_data.append(error_dict)
+    
+    # Wrap all response formated data (errors messages) into dict
+    # with key 'validation_error' and 'couriers'
+    response.data = {
+        'validation_error': {'couriers': formated_data},
+    }
+    return response
+
+
+def orders_exception_handler(response, context):
+    """
+    The order exception handler.
+    """
+    
+    request_data = context['request'].data['data']
+    
+    # Create new formated data
+    formated_data = []
+    for order_data, initial_errors in zip(request_data, response.data):
+        order_id = order_data.get('order_id')
+        if order_id and initial_errors:
+            error_dict = {'id': order_id}
+            print(initial_errors)
+            error_dict.update(initial_errors)
+            formated_data.append(error_dict)
+    
+    # Wrap all response formated data (errors messages) into dict
+    # with key 'validation_error' and 'orders'
+    response.data = {
+        'validation_error': {'orders': formated_data},
+    }
+    return response
     
